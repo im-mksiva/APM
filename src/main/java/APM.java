af109c215d09c438512e6d363d8e7ee8543122ab
mksiva <michelesivakumaran@gmail.com>
2022-04-08 12:18:18 +0200

    Aggiunta metodo insert nell'agent e test funzionamento

--- src/main/java/APM.java
+++ src/main/java/APM.java
@@ -5,16 +5,9 @@ import java.util.ArrayList;
 
 public class APM {
     public static void main(String[] args) {
-        passGen generatore = new passGen();
-        ArrayList<CharacterRule> test = new ArrayList<>();
-        test.add(new CharacterRule(EnglishCharacterData.Special));
-        test.add(new CharacterRule(EnglishCharacterData.Digit));
-        test.add(new CharacterRule(EnglishCharacterData.Alphabetical));
-        test.add(new CharacterRule(EnglishCharacterData.LowerCase));
-        test.add(new CharacterRule(EnglishCharacterData.UpperCase));
-
-        String password = generatore.genPass(22, test);
-        System.out.println(password);
+        SQLite_agent db_agent = new SQLite_agent();
+        db_agent.getValue("pietro");
+        db_agent.insertValue("Calogero");
     }
 }
 
--- src/main/java/SQLite_agent.java
+++ src/main/java/SQLite_agent.java
@@ -1,3 +1,44 @@
+import java.sql.*;
+
 public class SQLite_agent {
     // manages connection to sqlite db
-}
+    Connection connection = null;
+
+    SQLite_agent(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:sqlite:/home/mksiva/IdeaProjects/APM/APM/database/APM.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getValue(String nome){
+        try {
+            Statement statement = connection.createStatement();
+            String query = "select nome from prova where nome = '" + nome +"'";
+            // executeQuery() serve per recuperare dati
+            ResultSet rs = statement.executeQuery(query);
+            while(rs.next())
+            {
+//                 read the result set
+                System.out.println("name = " + rs.getString("nome"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void insertValue(String value){
+        try {
+            Statement statement = connection.createStatement();
+            String query = "insert into prova values ('" + value + "')";
+            System.out.println(query);
+            // executeUpdate() serve per aggiornare lo stato del database, che sia inserimento o cancellazione
+            statement.executeUpdate(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    }
