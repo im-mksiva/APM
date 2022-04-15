384d7edde9b74816e99ff38772a5ebb15a09fd1f
mksiva <michelesivakumaran@gmail.com>
2022-04-15 12:45:24 +0200

    update

---
+++ src/main/java/test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args){
+//        System.out.println("Vuoi effettuare la registrazione? s/n");
+//        Scanner scanner = new Scanner(System.in);
+//        String scelta = scanner.nextLine();
+        String username = "calogero";
+        String pass = "1234";
+//        System.out.print("Inserisci il nome utente: ");
+//        username = scanner.nextLine();
+//        System.out.print("Inserisci la password: ");
+//        pass = scanner.nextLine();
+
+//        AuthManager.Register(username, pass);
+//        AuthManager.Login(username,pass);
+        SQLite_agent db_agent = new SQLite_agent();
+//        db_agent.insertRecord("google.it","google","pippo","caaaaaaaa");
+
+
+
+//        db_init test = new db_init("1002"); //il file-db viene creato correttamente
+    }
+}
