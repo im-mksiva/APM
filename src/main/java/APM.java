02a020714351f09fff37869daa24d998224994a5
Calogeromor <102809875+Calogeromor@users.noreply.github.com>
2022-04-20 11:41:24 +0200

    import_csv

--- src/main/java/APM.java
+++ src/main/java/APM.java
@@ -1,11 +1,18 @@
 import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class APM {
     public static void main(String[] args) {
+        String percorso="C:\\Users\\calog\\IdeaProjects\\test\\src\\Password Chrome.csv";
+        try {
+            gestione_file_csv.import_file(percorso);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
